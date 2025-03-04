aed4f5323a8e47e278d7d82c76ec0a7068c6f53c
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2025-03-04 20:29:41 +0200

    Set-up express-async-errors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require("express");
 require("dotenv").config();
+require("express-async-errors")
+const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
 const connectDB = require("./db/connect");
