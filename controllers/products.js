d26442de1763163e9233d618dc82aa2fb051288b
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2025-03-04 22:48:08 +0200

    Testing products controller with products model

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,14 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("testing async-errors")
-  res.status(200).json({ msg: "products testing route" });
+  // throw new Error("testing async-errors");
+  // res.status(200).json({ msg: "products testing route" });
+
+  // const products = await Product.find({}) // get all products
+  // const products = await Product.find({ featured: true }); // get products by special criteria
+  
+  const products = await Product.find({ name: "vase table" }); // get products by special criteria
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
