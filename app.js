344af2b0fa56901cc63b54cd44d03083c0c2ed2b
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2025-03-04 20:15:03 +0200

    Set-up basic controllers and router

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const connectDB = require("./db/connect");
 const errorMiddleware = require("./middleware/error-handler");
 const notFoundMiddleware = require("./middleware/not-found");
+const productsRoute = require("./routes/products");
 
 //middleware
 app.use(express.json());
@@ -16,7 +17,8 @@ app.get("/", (req, res) => {
     .send("<h1>Store API</h1><a href='/api/v1/products'>Products Route</a>");
 });
 
-//Products Route
+//Products API
+app.use("/api/v1/products", productsRoute);
 
 //// Not-found middleware (for handling 404 errors)
 app.use(notFoundMiddleware);
@@ -26,7 +28,7 @@ app.use(errorMiddleware);
 const start = async () => {
   try {
     //Connect to DB
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(
       port,
       console.log(`Server running on port http://localhost:${port}`)
