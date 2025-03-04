ccbe99f1c194f2a86d3c76bf0977d939784aa2b1
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2025-03-04 22:11:00 +0200

    Add Products Schema and model

---
+++ models/product.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const productsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "PLease Prduct Name Must be Provided"],
+    },
+    price: {
+      type: Number,
+      required: [true, "PLease Prduct Price Must be Provided"],
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    company: {
+      type: String,
+      enum: {
+        values: ["ikea", "liddy", "caressa", "marcos"],
+        message: "{VALUE} is not supported",
+      },
+      // enum: ["ikea", "liddy", "caressa", "marcos"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productsSchema);
+
