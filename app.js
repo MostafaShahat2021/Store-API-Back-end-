2775309d5edf6332fc7cb1e01cfa8608d810cba9
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2025-03-04 19:37:46 +0200

    Update app.js to use notFound and error middleware

--- app.js
+++ app.js
@@ -2,15 +2,36 @@ const express = require("express");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 const app = express();
+const errorMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
 
 //middleware
 app.use(express.json());
 
 // routes
 app.get("/", (req, res) => {
-  res.status(200).send("<h1>Home page</h1>");
+  res
+    .status(200)
+    .send("<h1>Store API</h1><a href='/api/v1/products'>Products Route</a>");
 });
-app.listen(
-  port,
-  console.log(`Server running on port http://localhost:${port}`)
-);
+
+//Products Route
+
+//// Not-found middleware (for handling 404 errors)
+app.use(notFoundMiddleware);
+// Error-handling middleware (for handling all other errors)
+app.use(errorMiddleware);
+
+
+const start = async()=>{
+  try {
+    //Connect to DB
+    app.listen(
+      port,
+      console.log(`Server running on port http://localhost:${port}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+start()
--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,3 +1,8 @@
-const notFound = (req, res) => res.status(404).send('Route does not exist')
+const notFound = (req, res) =>
+  res
+    .status(404)
+    .send(
+      '<h1>Route does not exist</h1><a href="http://localhost:3000">Go back to Home</a>'
+    );
 
-module.exports = notFound
+module.exports = notFound;
