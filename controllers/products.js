d8ca588c54ec28640168f02ed99b4944427aad00
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2025-03-05 22:35:07 +0200

    Update getAllProducts Controller to use req.query

--- controllers/products.js
+++ controllers/products.js
@@ -6,17 +6,34 @@ const getAllProductsStatic = async (req, res) => {
 
   // const products = await Product.find({}) // get all products
   // const products = await Product.find({ featured: true }); // get products by special criteria
-  
-  const products = await Product.find({ name: "vase table" }); // get products by special criteria
+  // const products = await Product.find({ name: "vase table" }); // get products by special criteria
+
+  const products = await Product.find({}).sort("-name price"); // sort products from z to a and price from small to larg
   res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  // console.log(req.query); // ~/products?featured=true
+  // Sort ~/products?sort=name
+
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+
+  res.status(200).json({ success: true, count: products.length, products });
 };
 
 module.exports = {
   getAllProductsStatic,
   getAllProducts,
 };
-
