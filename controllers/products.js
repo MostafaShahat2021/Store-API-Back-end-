93e48843b6fc77ebd6db213878eaec101c37bfdf
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2025-03-10 07:21:49 +0200

    Added numeric filtering functionality to getAllProducts controller

--- controllers/products.js
+++ controllers/products.js
@@ -9,7 +9,10 @@ const getAllProductsStatic = async (req, res) => {
   // const products = await Product.find({}).sort("-name price"); // sort products from z to a and price from small to larg
   // const products = await Product.find({}).select("name price"); // serach for certain fields or select wiche fields to show in the products object
   // const products = await Product.find({}).select("name price").limit(4); // limit products to only 4 items to show on the list
-  const products = await Product.find({}).select("name price").limit(4).skip(5); // skip the first 5 items from the list
+  // const products = await Product.find({}).select("name price").limit(4).skip(5); // skip the first 5 items from the list
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort("price")
+    .select("name price"); // `$gt` operator is a MongoDB query to filter documents where the value of the `price` field is greater than 30.
   res.status(200).json({ nbHits: products.length, products });
 };
 
@@ -17,7 +20,7 @@ const getAllProducts = async (req, res) => {
   // console.log(req.query); // ~/products?featured=true
   // Sort ~/products?sort=name
 
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -28,7 +31,31 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  // console.log(queryObject);
+  //aggregation: Comparison operators
+  //Added numeric filtering functionality
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+      console.log(filters);
+    });
+  }
+  console.log(queryObject);
 
   // sort products please notice the syntax on line 11
   let result = Product.find(queryObject);
